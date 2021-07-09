9082fede172d1690449e2c6f3921a4f9c1de6760
James Heaney <james@Jamess-MacBook-Pro.local>
2021-07-08 21:53:10 -0400

    Removed personal website, added link to GitHub repo.

--- front-end/src/components/Navbar.js
+++ front-end/src/components/Navbar.js
@@ -3,10 +3,10 @@ import './css/Navbar.css';
 function Navbar() {
   return (
     <div className="Navbar">
-      <a rel="noreferrer" className="StripAnchor" href="https://jvheaney.com">
+      <a rel="noreferrer" className="StripAnchor" href="#">
         <span className="LogoText">adidas<span className="Bold">B2B</span></span>
       </a>
-      <a rel="noreferrer" target="_blank" className="StripAnchor" href="https://github.com/Jvheaney">
+      <a rel="noreferrer" target="_blank" className="StripAnchor" href="https://github.com/Jvheaney/adidas-b2b-demo">
         <span className="LogoText">View on <span className="Bold">GitHub</span></span>
       </a>
     </div>
